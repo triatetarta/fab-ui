66bb4141c847553c5415c64ddfb236c81b3e33d9
Dimitrios Chatziioannou <dimitrios@kmatech.co>
2025-11-25 22:07:26 +0000

    fix registry

--- registry/__index__.tsx
+++ registry/__index__.tsx
@@ -390,6 +390,30 @@ export const Index: Record<string, any> = {
     categories: undefined,
     meta: undefined,
   },
+  slider: {
+    name: 'slider',
+    description: '',
+    type: 'registry:ui',
+    registryDependencies: undefined,
+    files: [
+      {
+        path: 'registry/default/ui/slider.tsx',
+        type: 'registry:ui',
+        target: '',
+      },
+    ],
+    component: React.lazy(async () => {
+      const mod = await import('@/registry/default/ui/slider.tsx');
+      const exportName =
+        Object.keys(mod).find(
+          (key) =>
+            typeof mod[key] === 'function' || typeof mod[key] === 'object'
+        ) || item.name;
+      return { default: mod.default || mod[exportName] };
+    }),
+    categories: undefined,
+    meta: undefined,
+  },
   switch: {
     name: 'switch',
     description: '',
