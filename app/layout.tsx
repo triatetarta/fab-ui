907fe73ad9ecfd1b7d77545d9f7ad401fb0eeeb0
Dimitrios Chatziioannou <dimitrios@kmatech.co>
2025-11-25 19:49:36 +0000

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,26 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({
+import './global.css';
+
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Geist } from 'next/font/google';
+
+const geist = Geist({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+    <html lang='en' className={geist.className} suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <div className='root flex min-h-screen flex-col'>
+          <RootProvider>
+            {children}
+            <SpeedInsights />
+            <Analytics />
+          </RootProvider>
+        </div>
       </body>
     </html>
   );
