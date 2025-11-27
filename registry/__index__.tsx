793903b7b4e59471803f618dad52c531660f9a1e
Dimitrios Chatziioannou <52290418+triatetarta@users.noreply.github.com>
2025-11-27 19:11:19 +0000

    adding menu component (#21)
    Co-authored-by: Dimitrios Chatziioannou <dimitrios@kmatech.co>

--- registry/__index__.tsx
+++ registry/__index__.tsx
@@ -318,6 +318,30 @@ export const Index: Record<string, any> = {
     categories: undefined,
     meta: undefined,
   },
+  menu: {
+    name: 'menu',
+    description: '',
+    type: 'registry:ui',
+    registryDependencies: undefined,
+    files: [
+      {
+        path: 'registry/default/ui/menu.tsx',
+        type: 'registry:ui',
+        target: '',
+      },
+    ],
+    component: React.lazy(async () => {
+      const mod = await import('@/registry/default/ui/menu.tsx');
+      const exportName =
+        Object.keys(mod).find(
+          (key) =>
+            typeof mod[key] === 'function' || typeof mod[key] === 'object'
+        ) || item.name;
+      return { default: mod.default || mod[exportName] };
+    }),
+    categories: undefined,
+    meta: undefined,
+  },
   meter: {
     name: 'meter',
     description: '',
