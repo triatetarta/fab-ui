ba226d77425ee3d0d7d543930796e0db45c25709
Dimitrios Chatziioannou <52290418+triatetarta@users.noreply.github.com>
2025-11-30 11:00:38 +0000

    adding tabs component (#35)
    Co-authored-by: Dimitrios Chatziioannou <dimitrios@kmatech.co>

---
+++ registry/default/ui/tabs.tsx
@@ -0,0 +1,66 @@
+import { Tabs as TabsPrimitive } from '@base-ui-components/react/tabs';
+
+import { cn } from '@/lib/utils';
+
+function Tabs({ className, ...props }: TabsPrimitive.Root.Props) {
+  return (
+    <TabsPrimitive.Root
+      data-slot='tabs'
+      className={cn(
+        'flex flex-col gap-2 data-[orientation=vertical]:flex-row',
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function TabsList({ className, children, ...props }: TabsPrimitive.List.Props) {
+  return (
+    <TabsPrimitive.List
+      data-slot='tabs-list'
+      className={cn(
+        'relative z-0 flex w-fit items-center justify-center gap-x-0.5 rounded-lg bg-muted p-0.5 text-foreground',
+        'data-[orientation=vertical]:flex-col',
+        className
+      )}
+      {...props}
+    >
+      {children}
+      <TabsPrimitive.Indicator
+        data-slot='tab-indicator'
+        className={cn(
+          'absolute bottom-0 left-0 h-(--active-tab-height) w-(--active-tab-width) translate-x-(--active-tab-left) -translate-y-(--active-tab-bottom) transition-[width,translate] duration-200 ease-in-out',
+          '-z-1 rounded-md bg-background shadow-sm dark:bg-accent'
+        )}
+      />
+    </TabsPrimitive.List>
+  );
+}
+
+function TabsTrigger({ className, ...props }: TabsPrimitive.Tab.Props) {
+  return (
+    <TabsPrimitive.Tab
+      data-slot='tabs-trigger'
+      className={cn(
+        "flex flex-1 shrink-0 cursor-pointer items-center justify-center rounded-md border border-transparent text-sm font-medium whitespace-nowrap transition-[color,background-color,box-shadow] outline-none focus-visible:ring-2 focus-visible:ring-ring data-disabled:pointer-events-none data-disabled:opacity-64 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        'gap-1.5 px-[calc(--spacing(2.5)-1px)] py-[calc(--spacing(1.5)-1px)]',
+        'data-[orientation=vertical]:w-full data-[orientation=vertical]:justify-start',
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+function TabsPanel({ className, ...props }: TabsPrimitive.Panel.Props) {
+  return (
+    <TabsPrimitive.Panel
+      data-slot='tabs-content'
+      className={cn('flex-1 outline-none', className)}
+      {...props}
+    />
+  );
+}
+
+export { Tabs, TabsList, TabsTrigger, TabsPanel };
