b6939a08972e0dc0b438bba804a002c60fe7d7f0
Dimitrios Chatziioannou <52290418+triatetarta@users.noreply.github.com>
2025-12-31 15:35:09 +0000

    general style fixes (#113)
    Co-authored-by: Dimitrios Chatziioannou <dimitrios@kmatech.co>

--- registry/default/examples/checkbox-group-with-field.tsx
+++ registry/default/examples/checkbox-group-with-field.tsx
@@ -9,25 +9,27 @@ import { Label } from '@/registry/default/ui/label';
 
 export function CheckboxGroupWithField() {
   return (
-    <Field>
-      <FieldLabel>Email Preferences</FieldLabel>
-      <FieldDescription>
-        Choose which emails you&apos;d like to receive.
-      </FieldDescription>
-      <CheckboxGroup className='mt-2'>
-        <Label>
-          <Checkbox name='emails' value='marketing' />
-          Marketing emails
-        </Label>
-        <Label>
-          <Checkbox name='emails' value='social' />
-          Social notifications
-        </Label>
-        <Label>
-          <Checkbox name='emails' value='security' />
-          Security alerts
-        </Label>
-      </CheckboxGroup>
-    </Field>
+    <div>
+      <Field>
+        <FieldLabel>Email Preferences</FieldLabel>
+        <FieldDescription>
+          Choose which emails you&apos;d like to receive.
+        </FieldDescription>
+        <CheckboxGroup className='mt-2'>
+          <Label>
+            <Checkbox name='emails' value='marketing' />
+            Marketing emails
+          </Label>
+          <Label>
+            <Checkbox name='emails' value='social' />
+            Social notifications
+          </Label>
+          <Label>
+            <Checkbox name='emails' value='security' />
+            Security alerts
+          </Label>
+        </CheckboxGroup>
+      </Field>
+    </div>
   );
 }
